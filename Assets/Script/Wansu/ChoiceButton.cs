97b2958b47be3181bd1f9a94784969bae8d2f8ba
okjunges <rladhkstn02@gmail.com>
2025-05-11 21:52:25 +0900

    1Date Stroy Text, Not Img

--- Assets/Script/Wansu/ChoiceButton.cs
+++ Assets/Script/Wansu/ChoiceButton.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChoiceButton : MonoBehaviour
 {
+    public Text choiceText;
+    public string[] choiceStr;
     // 정답 : 0 / 오답 : 1, 2
     public int type;
+    void OnEnable()
+    {
+        choiceText.text = choiceStr[MorningManager.Instance.date - 1];
+    }
     public void Choiced()
     {
         GetComponentInParent<NpcUi>().OnChoiceButtonClicked(type);
